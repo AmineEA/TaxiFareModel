19d40f327d3c89756bd42cb74bacbcbe4888643e
Amine El Amrani <amine.elamrani.epfl@gmail.com>
2021-08-17 16:09:02 +0200

    challenges complete

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,13 +1,25 @@
+from ml_flow_test import EXPERIMENT_NAME
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.data import get_data, clean_data
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = '[DE] [Berlin] [AmineEA] TaxiFareModel 1.0'
+
 
 class Trainer():
-    def __init__(self, X, y):
+
+
+
+    def __init__(self, X, y, Regressor):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -15,6 +27,31 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.Regressor = Regressor
+        self.experiment_name = EXPERIMENT_NAME
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    # @memoized_property
+    def mlflow_run(self):
+        self.mlflow_run = self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -33,7 +70,7 @@ class Trainer():
         ], remainder="drop")
         self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('model', self.Regressor)
         ])
 
 
@@ -61,12 +98,20 @@ if __name__ == "__main__":
     # hold out
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    # train
-    trainer = Trainer(X_train, y_train)
+    # train, evaluate for each model
+    models = {
+        'linear' : LinearRegression(),
+        'RandomForest' : RandomForestRegressor()
+    }
 
-    # evaluate
-    trainer.run()
-    trainer.evaluate(X_test, y_test)
+    for title, model in models.items():
+        trainer = Trainer(X_train, y_train, model)
 
-    print(trainer.__dict__)
+        trainer.run()
+        trainer.evaluate(X_test, y_test)
+        print(f'{title} : {trainer.score}')
+
+        trainer.mlflow_run()
+        trainer.mlflow_log_metric("rmse", trainer.score)
+        trainer.mlflow_log_param("model", title)
 
